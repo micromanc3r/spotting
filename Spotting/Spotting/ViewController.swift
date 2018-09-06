1cbe130cf7e195651007559e6cf8772f9096245d
micromanc3r <micromanc3r@gmail.com>
2018-09-06 20:28:34 +0200

    Create project
    Create new project for the Spotting app

---
+++ Spotting/Spotting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spotting
+//
+//  Created by micromanc3r on 06/09/2018.
+//  Copyright © 2018 micromanc3r. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
