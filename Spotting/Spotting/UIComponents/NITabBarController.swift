54cb10648fe73519424389e43cd66168c3e95d90
micromanc3r <micromanc3r@gmail.com>
2018-09-14 08:29:11 +0200

    NITabBarController

---
+++ Spotting/Spotting/UIComponents/NITabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  NITabBarController.swift
+//  Spotting
+//
+//  Created by micromanc3r on 14/09/2018.
+//  Copyright © 2018 micromanc3r. All rights reserved.
+//
+
+import UIKit
+
+class NITabBarController: UITabBarController {
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("Unavailable")
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+}
