752aac1283be3f3f582de098091f9982a3bdf99b
micromanc3r <micromanc3r@gmail.com>
2018-09-29 12:33:14 +0200

    Privacy UILayout init

--- Spotting/Spotting/MainView/Settings/PrivacyPolicy/PrivacyPolicyViewController.swift
+++ Spotting/Spotting/MainView/Settings/PrivacyPolicy/PrivacyPolicyViewController.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2018 micromanc3r. All rights reserved.
 //
 
+import Cartography
 import Foundation
 import UIKit
 
 class PrivacyPolicyViewController: NIViewController {
+    let privacyTextView = UITextView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareLayout()
@@ -18,6 +21,23 @@ class PrivacyPolicyViewController: NIViewController {
 
 extension PrivacyPolicyViewController {
     private func prepareLayout() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        title = R.string.localizable.settings_privacy_policy()
+
+        preparePrivacyTextView()
+
+        constrain(view.safeAreaLayoutGuide,
+                  privacyTextView) { superview, privacyTextView in
+            privacyTextView.top == superview.top
+            privacyTextView.left == superview.left
+            privacyTextView.right == superview.right
+            privacyTextView.bottom == superview.bottom
+        }
+    }
+
+    private func preparePrivacyTextView() {
+        privacyTextView.text = "Privacy Text"
+
+        view.addSubview(privacyTextView)
     }
 }
