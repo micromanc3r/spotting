3c0f036a6e356c4f21fe71d63a36048c6e75b565
micromanc3r <micromanc3r@gmail.com>
2018-10-06 22:55:41 +0200

    SpotDetailViewController

---
+++ Spotting/Spotting/Spots/SpotDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SpotDetailViewController.swift
+//  Spotting
+//
+//  Created by micromanc3r on 06/10/2018.
+//  Copyright © 2018 micromanc3r. All rights reserved.
+//
+
+import Cartography
+import UIKit
+
+class SpotDetailViewController: NIViewController {
+    let spot: Spot
+
+    let spotImageView = UIImageView()
+    let titleLabel = UILabel()
+
+    init(withSpot spot: Spot) {
+        self.spot = spot
+        super.init()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        prepareLayout()
+    }
+}
+
+extension SpotDetailViewController {
+    private func prepareLayout() {
+        view.backgroundColor = .white
+
+        constrain(view, spotImageView, titleLabel) { superview, spotImageView, titleLabel in
+            spotImageView.top == superview.top
+            spotImageView.left == superview.left
+            spotImageView.left == superview.right
+            spotImageView.height == spotImageView.width
+
+            titleLabel.bottom == spotImageView.bottom - 16
+            titleLabel.left == spotImageView.left + 16
+            titleLabel.right == spotImageView.right - 16
+        }
+    }
+
+    private func prepareSpotImage() {
+        spotImageView.image = spot.image
+        view.addSubview(spotImageView)
+    }
+
+    private func prepareSpotTitle() {
+        titleLabel.text = spot.title
+        view.addSubview(titleLabel)
+    }
+}
