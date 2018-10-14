e7a3da315cce15acfa5ac431bb5556aea7b78c94
micromanc3r <micromanc3r@gmail.com>
2018-10-14 17:53:18 +0200

    Add NewSpotViewController

---
+++ Spotting/Spotting/NewSpot/NewSpotViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewSpotViewController.swift
+//  Spotting
+//
+//  Created by micromanc3r on 14/10/2018.
+//  Copyright © 2018 micromanc3r. All rights reserved.
+//
+
+import Foundation
+
+class NewSpotViewController: NIViewController {
+    override init() {
+        super.init()
+        prepareLayout()
+    }
+}
+
+extension NewSpotViewController {
+    private func prepareLayout() {}
+}
